907ab5fbc6777ef4533b5b0ffe648e1409a626be
Simone Marzulli <smarzulli@guidewire.com>
2019-05-17 17:07:07 +0100

    add blurred logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import ReactSVG from 'react-svg';
+
 import './App.css'
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -12,33 +16,54 @@ class App extends Component {
   componentDidMount() {
     const logo_structure_url = 'https://logos-c87b5.firebaseio.com/items.json';
 
-    fetch(logo_structure_url)
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
+    axios.get(logo_structure_url)
+      .then(res => {
         const getRandomInt = (max) => {
-            return Math.floor(Math.random() * Math.floor(max));
+          return Math.floor(Math.random() * Math.floor(max));
         }
 
-        const logos_structure = data;
+        const logos_structure = res.data;
         const random_logo = logos_structure[Object.keys(logos_structure)[getRandomInt(Object.keys(logos_structure).length)]];
-        const logo_img_base_url = `https://cdn.svgporn.com/logos/${random_logo.files[0]}`
+        const logo_img_url = `https://cdn.svgporn.com/logos/${random_logo.files[0]}`
 
-        this.setState({ data: logo_img_base_url });
-      })
-      .catch(err => {
-        console.err(`There was an error: ${err}`);
-      })
+        this.setState({
+            logo_img_url,
+            blurred: true
+        });
+      });
+  }
+
+  renderLogo() {
+    const {blurred, logo_img_url} = this.state
+
+    if (!!logo_img_url) {
+      return (
+        <ReactSVG
+          src={logo_img_url}
+          beforeInjection={svg => {
+            if (blurred) {
+              svg.setAttribute('style', 'filter: blur(15px)');
+            } else {
+              svg.setAttribute('style', 'filter: blur(0px)');
+            }
+          }}
+          onClick={() => {
+            this.setState({
+              blurred: false
+            });
+          }}
+        />
+      );
+    }
+
+    return <h1>Loading...</h1>
   }
 
   render() {
-    const { data } = this.state;
-
     return (
       <div className="game">
-        <h1>Guess-nerd-logo 1.0</h1>
-        <img src={data} alt="loading logo..."/>
+        <h1><span role="img" aria-label="bullseye">🎯</span> Guess Nerd Logo 1.0</h1>
+        {this.renderLogo()}
       </div>
     );
   }
