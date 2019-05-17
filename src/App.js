588e15286c95b419c3d06b95140035ed8fcba4b6
Simone Marzulli <smarzulli@guidewire.com>
2019-05-17 11:30:36 +0100

    first commit created a MVP for the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.css'
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.state = {
+      data: null,
+    };
+  }
+
+  componentDidMount() {
+    const logo_structure_url = 'https://logos-c87b5.firebaseio.com/items.json';
+
+    fetch(logo_structure_url)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        const getRandomInt = (max) => {
+            return Math.floor(Math.random() * Math.floor(max));
+        }
+
+        const logos_structure = data;
+        const random_logo = logos_structure[Object.keys(logos_structure)[getRandomInt(Object.keys(logos_structure).length)]];
+        const logo_img_base_url = `https://cdn.svgporn.com/logos/${random_logo.files[0]}`
+
+        this.setState({ data: logo_img_base_url });
+      })
+      .catch(err => {
+        console.err(`There was an error: ${err}`);
+      })
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div className="game">
+        <h1>Guess-nerd-logo 1.0</h1>
+        <img src={data} alt="loading logo..."/>
+      </div>
+    );
+  }
 }
 
 export default App;
