fee40b936d3d6c7b680a3a21e8a263119a802d5d
Simone Marzulli <smarzulli@guidewire.com>
2019-06-07 14:50:33 +0100

    Fixed ESLint issues

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactSVG from 'react-svg';
-import { Cutout, Hourglass } from 'react95'
+import { Cutout, Hourglass } from 'react95';
 
 import './Logo.css';
 
 class Logo extends Component {
+  renderHourGlass = () => <Hourglass size={ 125 } />;
+
+  applyBlurFilter = (svg) => {
+    const { blurred } = this.props;
+    const svgWidth = parseInt(svg.attributes.width.nodeValue, 10);
+    const svgHeight = parseInt(svg.attributes.height.nodeValue, 10);
+
+    if (blurred) {
+      svg.setAttribute('style', 'filter: blur(8px)');
+    }
+
+    if (svgWidth > 256) {
+      // if width is more than 256px most likely the logo is composed
+      // of a written text so increase the blur filter
+      if (blurred) {
+        svg.setAttribute('style', 'filter: blur(10px)');
+      }
+
+      svg.setAttribute('width', '256px');
+    }
+
+    if (svgHeight > 256) {
+      svg.setAttribute('height', '256px');
+    }
+  }
+
   render() {
-    const {blurred, url, score, fallbackError} = this.props
-    
+    const { url, score, fallbackError } = this.props;
+
     let cssClasses = 'logo ';
 
     if (score >= 30) {
@@ -21,32 +47,11 @@ class Logo extends Component {
     return (
       <Cutout className='logo_container'>
         <ReactSVG
-          className={cssClasses} 
-          src={url}
-          loading={() => <Hourglass size={125} />}
-          fallback={() => fallbackError()}
-          beforeInjection={svg => {
-            const svgWidth = parseInt(svg.attributes.width.nodeValue);
-            const svgHeight = parseInt(svg.attributes.height.nodeValue);
-
-            if (blurred) {
-              svg.setAttribute('style', 'filter: blur(8px)')
-            }
-
-            if (svgWidth > 256) {
-              // if width is more than 256px most likely the logo is composed
-              // of a written text so increase the blur filter
-              if (blurred) {
-                svg.setAttribute('style', 'filter: blur(10px)')
-              }
-
-              svg.setAttribute('width', '256px');
-            }
-
-            if (svgHeight > 256) {
-              svg.setAttribute('height', '256px');
-            }
-          }}
+          className={ cssClasses }
+          src={ url }
+          loading={ this.renderHourGlass }
+          fallback={ fallbackError }
+          beforeInjection={ this.applyBlurFilter }
         />
       </Cutout>
     );
--- src/components/SoundEffects.js
+++ src/components/SoundEffects.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import buttonClickSound from '../resources/sounds/buttonClick.mp3';
 import gameoverSound from '../resources/sounds/gameover.mp3';
@@ -6,12 +6,12 @@ import guessedSound from '../resources/sounds/guessed.mp3';
 
 class SoundEffects extends Component {
   render() {
-    const {muted} = this.props;
+    const { muted } = this.props;
     return (
       <span>
-        <audio id='buttonClickSound' src={buttonClickSound} type="audio/mpeg" muted={muted}/>
-        <audio id='guessedSound' src={guessedSound} type="audio/mpeg" muted={muted}/>
-        <audio id='gameoverSound' src={gameoverSound} type="audio/mpeg" muted={muted}/>
+        <audio id='buttonClickSound' src={ buttonClickSound } type="audio/mpeg" muted={ muted }/>
+        <audio id='guessedSound' src={ guessedSound } type="audio/mpeg" muted={ muted }/>
+        <audio id='gameoverSound' src={ gameoverSound } type="audio/mpeg" muted={ muted }/>
       </span>
     );
   }
