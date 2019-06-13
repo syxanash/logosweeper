ec6f77cae941b1a9093a05a14e162e8b2ee7b3d7
Simone Marzulli <smarzulli@guidewire.com>
2019-06-13 16:25:15 +0100

    show twitter button only if score is > 0

--- src/components/AdditionalInfo.js
+++ src/components/AdditionalInfo.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { Fieldset, Anchor, Button } from 'react95';
+import 'animate.css';
 
 import twitterLogo from '../resources/images/twitter.svg';
 
 class AdditionalInfo extends Component {
+  renderShareButton = () => {
+    const { score, timeCount } = this.props;
+    return (
+      <Button size='lg' className='animated rubberBand'>
+        <Anchor style={ { textDecoration: 'none' } } href={ `https://twitter.com/intent/tweet?text=I%20just%20guessed%20${score}%20logos%20in%20${timeCount}%20seconds%20on%20https%3A%2F%2Flogosweeper.surge.sh%20%F0%9F%8E%AF` } target='_blank'>
+          <span style={ { display: 'flex' } }>Share score on&nbsp;<img src={ twitterLogo } style={ { height: '25px' } } alt="twitter"/></span>
+        </Anchor>
+      </Button>
+    );
+  }
+
   render() {
     const {
-      logo, gameover, score, timeCount,
+      logo, gameover, score,
     } = this.props;
 
     if (gameover) {
@@ -14,11 +26,7 @@ class AdditionalInfo extends Component {
         <Fieldset>
           <h1 style={ { color: 'red' } }>GAMEOVER!</h1>
           <h2>The logo was from <Anchor href={ logo.url } target='_blank'>{logo.name}</Anchor></h2>
-          <Button size='lg'>
-            <Anchor style={ { textDecoration: 'none' } } href={ `https://twitter.com/intent/tweet?text=I%20just%20guessed%20${score}%20logos%20in%20${timeCount}%20seconds%20on%20https%3A%2F%2Flogosweeper.surge.sh%20%F0%9F%8E%AF` } target='_blank'>
-              <span style={ { display: 'flex' } }>Share score on&nbsp;<img src={ twitterLogo } style={ { height: '25px' } } alt="twitter"/></span>
-            </Anchor>
-          </Button>
+          { score > 0 ? this.renderShareButton() : '' }
         </Fieldset>
       );
     }
