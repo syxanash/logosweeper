7c9667f56d9d199d86874b621069f6d789f21d61
Simone Marzulli <smarzulli@guidewire.com>
2019-06-15 10:54:19 +0100

    Updated InfoWindow twitter link and add ProductHunt logo

--- src/components/InfoWindow.js
+++ src/components/InfoWindow.js
@@ -10,6 +10,7 @@ import './InfoWindow.css';
 
 import octocatLogo from '../resources/images/octocat.png';
 import twitterLogo from '../resources/images/twitter.svg';
+import productHuntLogo from '../resources/images/product-hunt.png';
 
 class InfoWindow extends Component {
   render() {
@@ -26,7 +27,7 @@ class InfoWindow extends Component {
                 <span role='img' aria-label='logo'>🎯</span> About
               </WindowHeader>
               <WindowContent className='window_content'>
-                <div style={ { textAlign: 'left', paddingBottom: '15px' } }>Test your wisdom and readiness to adopt the next big bloated tech stack by guessing as many logos as you can in a row.<br /><br /><i>All logos appearing on this app are property of their respective owners.</i><br /><br />Made with <div className='animated heartBeat delay-5s' style={ { display: 'inline-block' } }><span role='img' aria-label='love'>❤️</span></div> and <span role="img" aria-label="beer">🍺</span> by <b>syx</b></div>
+                <div style={ { textAlign: 'left', paddingBottom: '20px' } }>Test your wisdom and readiness to adopt the next big bloated tech stack by guessing as many logos as you can in a row.<br /><br /><i>All logos appearing on this app are property of their respective owners.</i><br /><br />Made with <div className='animated heartBeat delay-5s' style={ { display: 'inline-block' } }><span role='img' aria-label='love'>❤️</span></div> and <span role="img" aria-label="beer">🍺</span> by <b>syx</b></div>
                 <div className='action_buttons_container'>
                   <Button size='lg' square style={ { margin: '5px' } }>
                     <Anchor href='https://github.com/syxanash/logosweeper' target='_blank'>
@@ -34,7 +35,12 @@ class InfoWindow extends Component {
                     </Anchor>
                   </Button>
                   <Button size='lg' square style={ { margin: '5px' } }>
-                    <Anchor href='https://twitter.com/intent/tweet?text=Test%20how%20wise%20and%20knowledgeable%20you%20are%20about%20new%20technologies%20by%20guessing%20as%20many%20logos%20as%20you%20can%20in%20a%20row%20%F0%9F%8E%AF%20https%3A%2F%2Flogosweeper.surge.sh' target='_blank'>
+                    <Anchor href='https://www.producthunt.com/posts/logosweeper' target='_blank'>
+                      <img src={ productHuntLogo } style={ { height: '30px' } } alt="product-hunt"/>
+                    </Anchor>
+                  </Button>
+                  <Button size='lg' square style={ { margin: '5px' } }>
+                    <Anchor href='https://twitter.com/intent/tweet?text=Test%20your%20wisdom%20and%20readiness%20to%20adopt%20the%20next%20big%20bloated%20tech%20stack%20by%20guessing%20as%20many%20logos%20as%20you%20can%20in%20a%20row%20%F0%9F%8E%AF%20https%3A%2F%2Flogosweeper.surge.sh' target='_blank'>
                       <img src={ twitterLogo } style={ { height: '30px' } } alt="twitter"/>
                     </Anchor>
                   </Button>
